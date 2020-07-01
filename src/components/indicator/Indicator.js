d328996b67b3eb7631b50fd5ab453651534930fc
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-07-01 19:39:59 -0300

    Create Indicator component

---
+++ src/components/indicator/Indicator.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Indicator.module.css';
+
+function Indicator(props) {
+  function getClassName() {
+    let className = [styles.container];
+    if (!isSelected && isSelectable) {
+      className.push(styles.hover);
+    }
+    if (isSelectable) {
+      className.push(styles.cursor);
+    }
+    if (isSelected) {
+      className.push(styles.selected);
+    }
+    return className.join(' ');
+  }
+
+  const {
+    value,
+    title,
+    subtitle,
+    color = 'blue',
+    isSelected = false,
+    isSelectable = false,
+  } = props;
+
+  return (
+    <span className={getClassName()}>
+      <div className={styles.value} style={{ backgroundColor: color }}>
+        {value}
+      </div>
+      <div className={styles.text}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subtitle}>{subtitle}</div>
+      </div>
+    </span>
+  );
+}
+
+export default Indicator;
