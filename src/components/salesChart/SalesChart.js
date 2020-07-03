dc645468eb9d6e667c25bfe178e413a89590319a
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-07-03 12:42:29 -0300

    Create SalesChart component - There is a bug on ChartJs lib when the window is resizes (refer to: https://github.com/jtblin/angular-chart.js/issues/614#issuecomment-509649618 to know about Canvas.css)

---
+++ src/components/salesChart/SalesChart.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from './SalesChart.module.css';
+import Card from '../card';
+import { Bar } from 'react-chartjs-2';
+import './Canvas.css';
+
+function SalesChart(props) {
+  const { data } = props;
+
+  function formatCurrency(value) {
+    if (parseInt(value) >= 1000) {
+      return 'R$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    } else {
+      return 'R$' + value;
+    }
+  }
+
+  const chartData = {
+    labels: data.map((item) => item.month),
+    datasets: [
+      {
+        label: 'Vendas',
+        backgroundColor: 'green',
+        borderColor: 'darkgreen',
+        borderWidth: 1,
+        hoverBackgroundColor: 'darkgreen',
+        data: data.map((item) => item.value),
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+          if (label) {
+            label += ': ';
+          }
+          label += formatCurrency(tooltipItem.yLabel);
+          return label;
+        },
+      },
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            callback: function (value, index, values) {
+              return formatCurrency(value);
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <Card title="Vendas">
+      <Bar data={chartData} options={options} />
+    </Card>
+  );
+}
+
+export default SalesChart;
